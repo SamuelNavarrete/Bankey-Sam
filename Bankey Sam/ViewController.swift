9f2ebda164237f902759ae472f861aa16583c897
Samuel Fuentes <samuel.fn331@gmail.com>
2023-01-30 00:07:02 -0600

    Initial Commit

---
+++ Bankey Sam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey Sam
+//
+//  Created by Samuel Fuentes Navarrete on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
