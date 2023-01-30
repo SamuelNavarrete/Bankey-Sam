0356e18915195a08b9c1ab98eb3300db24bc6964
Samuel Fuentes <samuel.fn331@gmail.com>
2023-01-30 00:55:33 -0600

    Added the line to see the view

--- Bankey Sam/AppDelegate.swift
+++ Bankey Sam/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        
+        //window?.rootViewController = LoginViewController()
+
         return true
     }
 
